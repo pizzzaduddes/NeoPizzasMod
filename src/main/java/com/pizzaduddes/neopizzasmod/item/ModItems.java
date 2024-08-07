9f917e57d1dcdfbc73a1c622502c34a77d3248df
pizzaduddes <Pizzzaduddes@gmail.com>
2024-08-07 13:27:49 +0300

    Changes

--- src/main/java/com/pizzaduddes/neopizzasmod/item/ModItems.java
+++ src/main/java/com/pizzaduddes/neopizzasmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package com.pizzaduddes.neopizzasmod.item;
 
 import com.pizzaduddes.neopizzasmod.NeoPizzasMod;
+import com.pizzaduddes.neopizzasmod.item.custom.AntiSmelterItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.neoforged.bus.api.IEventBus;
@@ -16,6 +17,9 @@ public class ModItems {
 
     public static final DeferredItem<Item> RAW_TANZANITE = ITEMS.register("raw_tanzanite",
             () -> new Item(new Item.Properties().rarity(Rarity.UNCOMMON)));
+
+    public static final DeferredItem<Item> ANTI_SMELTER = ITEMS.register("anti_smelter",
+            () -> new AntiSmelterItem(new Item.Properties().durability(256)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/com/pizzaduddes/neopizzasmod/item/custom/AntiSmelterItem.java
@@ -0,0 +1,34 @@
+package com.pizzaduddes.neopizzasmod.item.custom;
+
+import com.pizzaduddes.neopizzasmod.block.ModBlocks;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+
+import java.util.Objects;
+
+public class AntiSmelterItem extends Item {
+    public AntiSmelterItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        Level level = context.getLevel();
+
+        if(!level.isClientSide()) {
+            if(level.getBlockState(context.getClickedPos()).is(ModBlocks.TANZANITE_BLOCK)){
+                level.destroyBlock(context.getClickedPos(), false, context.getPlayer());
+                context.getItemInHand().hurtAndBreak(1, ((ServerLevel) level), ((ServerPlayer) context.getPlayer()),
+                item -> Objects.requireNonNull(context.getPlayer()).onEquippedItemBroken(item, EquipmentSlot.MAINHAND));
+                level.setBlock(context.getClickedPos(), ModBlocks.RAW_TANZANITE_BLOCK.get().defaultBlockState(), 3);
+                return InteractionResult.SUCCESS;
+            }
+        }
+        return InteractionResult.PASS;
+    }
+}
